da4b2f66a7153df51acc70deb375377ea75bc1c5
root <root@sd-100960.dedibox.fr>
2018-01-11 23:58:10 +0100

    update scan

--- scansiona.sh
+++ scansiona.sh
@@ -2,5 +2,15 @@
 
 # Media
 
-curl "https://docs.google.com/spreadsheets/d/13LgBSMgU4f268OLtVWqLqy3z3nbL--EWKuUetMTuD1E/export?gid=0&format=csv" | grep -v Domain | awk -F, '{ print $2}' > domini.csv
+curl "https://docs.google.com/spreadsheets/d/13LgBSMgU4f268OLtVWqLqy3z3nbL--EWKuUetMTuD1E/export?gid=0&format=csv" | grep -v Domain | awk -F, '{ print $2}' > media.csv
 
+/usr/local/bin/pshtt media.csv --output media-risultati.csv
+
+rm -f media.csv
+
+# Politica
+curl "https://docs.google.com/spreadsheets/d/13LgBSMgU4f268OLtVWqLqy3z3nbL--EWKuUetMTuD1E/export?gid=1181765951&format=csv" | grep -v Domain | awk -F, '{ print $2}' > politica.csv
+
+/usr/local/bin/pshtt politica.csv --output politica-risultati.csv
+
+rm -f politica.csv
