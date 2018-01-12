cc6d249b4c2a6f2d8d528ed30e41f3c5988e9f24
sicurezzadeimedia <root@sd-100960.dedibox.fr>
2018-01-12 08:21:55 +0100

    aggiungi output json

--- scansiona.sh
+++ scansiona.sh
@@ -11,7 +11,9 @@ git pull
 
 curl -s "https://docs.google.com/spreadsheets/d/13LgBSMgU4f268OLtVWqLqy3z3nbL--EWKuUetMTuD1E/export?gid=0&format=csv" | grep -v Domain | awk -F, '{ print $2}' > media.csv
 
-/usr/local/bin/pshtt media.csv --output media-risultati.csv
+/usr/local/bin/pshtt media.csv --output media-risultati.csv --sorted
+
+/usr/local/bin/pshtt media.csv --output media-risultati.json --json --sorted
 
 
 rm -f media.csv
@@ -19,7 +21,9 @@ rm -f media.csv
 # Politica
 curl -s "https://docs.google.com/spreadsheets/d/13LgBSMgU4f268OLtVWqLqy3z3nbL--EWKuUetMTuD1E/export?gid=1181765951&format=csv" | grep -v Domain | awk -F, '{ print $2}' > politica.csv
 
-/usr/local/bin/pshtt politica.csv --output politica-risultati.csv
+/usr/local/bin/pshtt politica.csv --output politica-risultati.csv --sorted
+
+/usr/local/bin/pshtt politica.csv --output politica-risultati.json --json --sorted
 
 rm -f politica.csv
 
