c1704610dc15f80a5e72b441d2f6445d97495c39
sicurezzadeimedia <root@sd-100960.dedibox.fr>
2018-01-12 09:18:50 +0100

    Riprova e rallenta su observatory scan

--- scansiona.sh
+++ scansiona.sh
@@ -20,9 +20,11 @@ curl -s "https://docs.google.com/spreadsheets/d/13LgBSMgU4f268OLtVWqLqy3z3nbL--E
 /usr/local/bin/pshtt media.csv --output media-risultati.csv 
 /usr/local/bin/pshtt media.csv --output media-risultati.json --json 
 
-# Mozilla HTTP Observatory scan
-for sito in `cat media.csv` ; do /usr/local/bin/observatory --rescan $sito; done
+# Mozilla HTTP Observatory scan - double pass
+for sito in `cat media.csv` ; do /usr/local/bin/observatory --rescan $sito ; sleep 5; done
+for sito in `cat media.csv` ; do /usr/local/bin/observatory --rescan $sito ; sleep 5; done
 
+# TODO: Comporre un CSV sintetico con i siti, rating e link ad analisi di dettaglio
 # Per etrarre i risultati sintetici del rating usare
 #  curl -s https://http-observatory.security.mozilla.org/api/v1/analyze?host=www.mozilla.org | jq -r '.grade'
 
@@ -36,8 +38,9 @@ curl -s "https://docs.google.com/spreadsheets/d/13LgBSMgU4f268OLtVWqLqy3z3nbL--E
 /usr/local/bin/pshtt politica.csv --output politica-risultati.csv
 /usr/local/bin/pshtt politica.csv --output politica-risultati.json --json 
 
-# Mozilla HTTP Observatory scan
-for sito in `cat politica.csv` ; do /usr/local/bin/observatory --rescan $sito; done
+# Mozilla HTTP Observatory scan - double pass
+for sito in `cat politica.csv` ; do /usr/local/bin/observatory --rescan $sito ; sleep 5; done
+for sito in `cat politica.csv` ; do /usr/local/bin/observatory --rescan $sito ; sleep 5; done
 
 rm -f politica.csv
 
